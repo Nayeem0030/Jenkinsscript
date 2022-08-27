99c78930b6bd51ff7623e9ee513522b1292042f8
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-08-27 15:38:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
   cidr_block           = "${var.vpc_cidr}"
-  enable_dns_hostnames = true
+  enable_dns_hostnames = true   #enabling hostname
   enable_dns_support   = true
 
   tags = {
@@ -19,3 +19,10 @@ resource "aws_vpc" "main" {
 
 # Creating Internet Gateway
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "my-test-igw"
+  }
+}
