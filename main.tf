5deb524c9d87210a061f526181b792977783f46a
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-08-27 13:26:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,94 +2,150 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_lb_target_group" "my-target-group" {
-  health_check {
-    interval            = 10
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-  }
+# Query all avilable Availibility Zone
+data "aws_availability_zones" "available" {}
 
-  name        = "my-test-tg"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "instance"
-  vpc_id      = "${var.vpc_id}"
-}
+# VPC Creation
 
-/*resource "aws_lb_target_group_attachment" "my-alb-target-group-attachment1" {
-  target_group_arn = "${aws_lb_target_group.my-target-group.arn}"
-  target_id        = "${var.instance1_id}"
-  port             = 80
-}
-resource "aws_lb_target_group_attachment" "my-alb-target-group-attachment2" {
-  target_group_arn = "${aws_lb_target_group.my-target-group.arn}"
-  target_id        = "${var.instance2_id}"
-  port             = 80
-}*/
-
-resource "aws_lb" "my-aws-alb" {
-  name     = "my-test-alb"
-  internal = false
-
-  security_groups = [
-    "${aws_security_group.my-alb-sg.id}",
-  ]
-
-  subnets = [
-    "${var.subnet1}",
-    "${var.subnet2}",
-  ]
+resource "aws_vpc" "main" {
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
-    Name = "my-test-alb"
-  }
-
-  ip_address_type    = "ipv4"
-  load_balancer_type = "application"
-}
-
-resource "aws_lb_listener" "my-test-alb-listner" {
-  load_balancer_arn = "${aws_lb.my-aws-alb.arn}"
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.my-target-group.arn}"
+    Name = "my-new-test-terraform-vpc"
   }
 }
 
-resource "aws_security_group" "my-alb-sg" {
-  name   = "my-alb-sg"
-  vpc_id = "${var.vpc_id}"
+# Creating Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "my-test-igw"
+  }
 }
 
-resource "aws_security_group_rule" "inbound_ssh" {
+# Public Route Table
+
+resource "aws_route_table" "public_route" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "my-test-public-route"
+  }
+}
+
+# Private Route Table
+
+resource "aws_default_route_table" "private_route" {
+  default_route_table_id = "${aws_vpc.main.default_route_table_id}"
+
+  route {
+    nat_gateway_id = "${aws_nat_gateway.my-test-nat-gateway.id}"
+    cidr_block     = "0.0.0.0/0"
+  }
+
+  tags = {
+    Name = "my-private-route-table"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+  count                   = 2
+  cidr_block              = "${var.public_cidrs[count.index]}"
+  vpc_id                  = "${aws_vpc.main.id}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+
+  tags = {
+    Name = "my-test-public-subnet.${count.index + 1}"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  count             = 2
+  cidr_block        = "${var.private_cidrs[count.index]}"
+  vpc_id            = "${aws_vpc.main.id}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+
+  tags = {
+    Name = "my-test-private-subnet.${count.index + 1}"
+  }
+}
+
+# Associate Public Subnet with Public Route Table
+resource "aws_route_table_association" "public_subnet_assoc" {
+  count          = 2
+  route_table_id = "${aws_route_table.public_route.id}"
+  subnet_id      = "${aws_subnet.public_subnet.*.id[count.index]}"
+  depends_on     = ["aws_route_table.public_route", "aws_subnet.public_subnet"]
+}
+
+# Associate Private Subnet with Private Route Table
+resource "aws_route_table_association" "private_subnet_assoc" {
+  count          = 2
+  route_table_id = "${aws_default_route_table.private_route.id}"
+  subnet_id      = "${aws_subnet.private_subnet.*.id[count.index]}"
+  depends_on     = ["aws_default_route_table.private_route", "aws_subnet.private_subnet"]
+}
+
+# Security Group Creation
+resource "aws_security_group" "test_sg" {
+  name   = "my-test-sg"
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+# Ingress Security Port 22
+resource "aws_security_group_rule" "ssh_inbound_access" {
   from_port         = 22
   protocol          = "tcp"
-  security_group_id = "${aws_security_group.my-alb-sg.id}"
+  security_group_id = "${aws_security_group.test_sg.id}"
   to_port           = 22
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "inbound_http" {
+resource "aws_security_group_rule" "http_inbound_access" {
   from_port         = 80
   protocol          = "tcp"
-  security_group_id = "${aws_security_group.my-alb-sg.id}"
+  security_group_id = "${aws_security_group.test_sg.id}"
   to_port           = 80
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "outbound_all" {
+# All OutBound Access
+resource "aws_security_group_rule" "all_outbound_access" {
   from_port         = 0
   protocol          = "-1"
-  security_group_id = "${aws_security_group.my-alb-sg.id}"
+  security_group_id = "${aws_security_group.test_sg.id}"
   to_port           = 0
   type              = "egress"
   cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_eip" "my-test-eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "my-test-nat-gateway" {
+  allocation_id = "${aws_eip.my-test-eip.id}"
+  subnet_id     = "${aws_subnet.public_subnet.0.id}"
+}
+
+# Adding Route for Transit Gateway
+
+resource "aws_route" "my-tgw-route" {
+  route_table_id         = "${aws_route_table.public_route.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_id     = "${var.transit_gateway}"
 }
